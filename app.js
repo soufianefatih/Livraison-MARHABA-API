45aeeb036bb4bbc8831ca8fa8ae0e45dc0104001
soufiane fatih <fatihe.soufiane@gmail.com>
2022-03-28 17:09:03 +0000

    installation package

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express ();
+
+
+const PORT = process.env.PORT || 5500;
+
+app.listen(PORT,()=>{
+console.log(`Server is running on port ${PORT}`);
+});
